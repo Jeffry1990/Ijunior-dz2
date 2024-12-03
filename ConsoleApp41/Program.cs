3a717f6e690e91b981859977ff1cec4204df0c0f
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-03 14:43:09 +0300

    Add files via upload

---
+++ ConsoleApp41/Program.cs
@@ -0,0 +1,15 @@
+﻿string name;
+string zodiacSign;
+string placeOfWork;
+int age;
+
+Console.Write("Ваше имя: ");
+name = Console.ReadLine();
+Console.Write("Ваш знак зодиака: ");
+zodiacSign = Console.ReadLine();
+Console.Write("Сколько вам лет: ");
+age = Convert.ToInt32(Console.ReadLine());
+Console.Write("Кем вы работаете: ");
+placeOfWork = Console.ReadLine();
+
+Console.WriteLine($"Вас зовут {name}, Вам {age}, Вы {zodiacSign} и Вы {placeOfWork}");
